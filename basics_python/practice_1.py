293d1b2032e2dce236a91e63f0fd1449fcb38f8f
Aktug Cilekci <aktugcilekci@hotmail.com>
2023-05-18 11:03:44 -0500

    New Practice

---
+++ basics_python/practice_1.py
@@ -0,0 +1,21 @@
+# Whats your Score 
+
+score = float (input("insert your overall:  "))
+
+if score >=89.5:
+
+    print("You get an A")
+
+elif score >= 79.5:
+    
+    print("Your get a B")
+
+elif score >= 69.5:
+
+    print("You get a C")
+
+elif score >= 59.5:
+    print(" You get the D")
+
+else : 
+    print ("You get a F")
